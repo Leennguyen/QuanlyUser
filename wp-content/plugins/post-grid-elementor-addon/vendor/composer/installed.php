2c80f37e4df74643e05aab2ae65a40a321024852
Leennguyen <nguyenthilinh26112002>
2022-10-10 17:49:15 +0700

    theme

---
+++ wp-content/plugins/post-grid-elementor-addon/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '144045abb5e43f1d635f2eb7cf9def902c873e57',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '144045abb5e43f1d635f2eb7cf9def902c873e57',
+            'dev_requirement' => false,
+        ),
+        'ernilambar/wp-admin-notice' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ernilambar/wp-admin-notice',
+            'aliases' => array(),
+            'reference' => '1fd14ff6d79994750d25b4f529284a6942e117eb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
